2072dea56d993a918f0f959db7fe472d5defe6fb
hammadx02 <hammadaali02@gmail.com>
2023-10-20 11:15:37 +0500

    updated

---
+++ lib/screens/google_places_api.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GooglePlacesApi extends StatefulWidget {
+  const GooglePlacesApi({super.key});
+
+  @override
+  State<GooglePlacesApi> createState() => _GooglePlacesApiState();
+}
+
+class _GooglePlacesApiState extends State<GooglePlacesApi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
