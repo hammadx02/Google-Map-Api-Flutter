4b50a5ea511a67caf8e8fc5ad7692f9fe6ce4b03
hammadx02 <hammadaali02@gmail.com>
2023-10-23 11:05:38 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue,
       ),
       home: const GetUserCurrentLocationScreen(),
     );
