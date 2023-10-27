e585d685542711f17fd0b40d73a0bf623b30858e
hammadx02 <hammadaali02@gmail.com>
2023-10-27 10:44:41 +0500

    updated

--- lib/screens/custom_marker_screen.dart
+++ lib/screens/custom_marker_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class CustomMarkerScreen extends StatefulWidget {
@@ -37,6 +38,40 @@ class _CustomMarkerScreenState extends State<CustomMarkerScreen> {
     target: LatLng(33.6910, 72.98072),
     zoom: 15,
   );
+
+  Future<Uint8List> getBytesFromAssets(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+        targetWidth: width);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    for (int i = 0; i < images.length; i++) {
+      final Uint8List markerIcon = await getBytesFromAssets(images[i], 100);
+      _markers.add(
+        Marker(
+          markerId: MarkerId(
+            i.toString(),
+          ),
+          position: _latLang[i],
+          icon: BitmapDescriptor.fromBytes(markerIcon),
+          infoWindow: const InfoWindow(title: 'Title of marker'),
+        ),
+      );
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
