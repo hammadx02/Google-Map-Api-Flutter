af9c4b1623752c958da7420bc52907ec04d95704
hammadx02 <hammadaali02@gmail.com>
2023-10-27 11:01:25 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_api/screens/custom_marker_info_window.dart';
+import 'package:google_maps_api/screens/polygon_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const CustomMarkerInfoWindowScreen(),
+      home: const PolygonScreen(),
     );
   }
 }
