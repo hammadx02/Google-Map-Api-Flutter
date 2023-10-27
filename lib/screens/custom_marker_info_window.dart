d209ed1779beb9faf26228d998e1674f3ad81c3c
hammadx02 <hammadaali02@gmail.com>
2023-10-27 10:56:27 +0500

    updated

---
+++ lib/screens/custom_marker_info_window.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomMarkerInfoWindowScreen extends StatefulWidget {
+  const CustomMarkerInfoWindowScreen({super.key});
+
+  @override
+  State<CustomMarkerInfoWindowScreen> createState() => _CustomInfoWindowScreenState();
+}
+
+class _CustomInfoWindowScreenState extends State<CustomMarkerInfoWindowScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
