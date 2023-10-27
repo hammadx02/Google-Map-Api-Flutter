5a3b77f3e328fc78cbe7246472a6ef0ef3cbb5f2
hammadx02 <hammadaali02@gmail.com>
2023-10-27 09:45:13 +0500

    updated

--- lib/screens/custom_marker_screen.dart
+++ lib/screens/custom_marker_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class CustomMarkerScreen extends StatefulWidget {
   const CustomMarkerScreen({super.key});
@@ -8,8 +11,48 @@ class CustomMarkerScreen extends StatefulWidget {
 }
 
 class _CustomMarkerScreenState extends State<CustomMarkerScreen> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  List<String> images = [
+    'images/car.png',
+    'images/car2.png',
+    'images/marker2.png',
+    'images/marker3.png',
+    'images/marker.png',
+    'images/motorcycle.png',
+  ];
+  final List<Marker> _markers = <Marker>[];
+  final List<LatLng> _latLang = <LatLng>[
+    const LatLng(33.6941, 72.9734),
+    const LatLng(33.7008, 72.9682),
+    const LatLng(33.6992, 72.9744),
+    const LatLng(33.6939, 72.9771),
+    const LatLng(33.6910, 72.9807),
+    const LatLng(33.7036, 72.9785),
+  ];
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(33.6910, 72.98072),
+    zoom: 15,
+  );
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Customer Marker'),
+      ),
+      body: SafeArea(
+        child: GoogleMap(
+          initialCameraPosition: _kGooglePlex,
+          mapType: MapType.normal,
+          myLocationButtonEnabled: true,
+          myLocationEnabled: true,
+          markers: Set<Marker>.of(_markers),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+      ),
+    );
   }
 }
